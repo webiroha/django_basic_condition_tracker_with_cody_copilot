a533d074090207ffb374c8479947e8f14cc6dbfb
webiroha <24987971+webiroha@users.noreply.github.com>
2025-03-21 21:34:07 +0900

    feat: add views test

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import SupplementRecord
 
-# Create your tests here.
+class ViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+
+    def test_login_view(self):
+        response = self.client.post(reverse('login'), {
+            'username': 'testuser',
+            'password': 'testpass123'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('supplement_record'))
